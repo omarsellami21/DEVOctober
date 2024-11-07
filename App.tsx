f91683f45ea36d9b25691394c6fdb399325d2ea4
unknown <osellami3@gmail.com>
2024-11-07 19:22:44 +0100

    2eme modifi

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
+import products from './src/data/products';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      
+      <FlatList
+        data={products}
+        renderItem={({item})=>
+      (
+        <Image 
+        source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/nike/nike1.png' }}
+        style={styles.Image}
+      />
+      )}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +28,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  Image:{
+    width:'100%',
+    aspectRatio:1,
+  }
 });
