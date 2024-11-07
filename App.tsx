df3b51be6ee41248f98fa297fc467add688ef24c
unknown <osellami3@gmail.com>
2024-11-07 19:34:12 +0100

    3eme modifi

--- App.tsx
+++ App.tsx
@@ -1,21 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
-import products from './src/data/products';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, FlatList } from "react-native";
+import products from "./src/data/products";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      
-      <FlatList
-        data={products}
-        renderItem={({item})=>
-      (
-        <Image 
-        source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/nike/nike1.png' }}
-        style={styles.Image}
-      />
-      )}
-      />
       <StatusBar style="auto" />
     </View>
   );
@@ -24,12 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  Image:{
-    width:'100%',
-    aspectRatio:1,
-  }
+  Image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
 });
